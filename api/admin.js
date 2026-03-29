ae6dccec4ae1265ed736197f2844c64359d7ab0f
yantoddddd <mn487gw0ywyd@animatimg.com>
2026-03-29 15:04:43 +0700

    Update admin.js

--- api/admin.js
+++ api/admin.js
@@ -1,2 +1,12 @@
-// admin.js
-const ADMIN_PASSWORD = "botakganteng"; // Ganti password sesuka kamu
+// api/admin.js
+// Password admin disimpan di backend, gak keliatan di frontend
+export default function handler(req, res) {
+    const { password } = req.query;
+    const ADMIN_PASSWORD = "botakganteng"; // Ganti password sesuka kamu
+    
+    if(password === ADMIN_PASSWORD){
+        res.status(200).json({ success: true });
+    } else {
+        res.status(401).json({ success: false });
+    }
+}
